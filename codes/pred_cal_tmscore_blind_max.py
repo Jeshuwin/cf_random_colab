13b1a379693e65240b471da78c047ffbded2f430
Myeongsang (Samuel) Lee <99306709+mslee1220@users.noreply.github.com>
2024-10-31 10:14:01 -0400

    Update pred_cal_tmscore_blind_max.py

--- codes/pred_cal_tmscore_blind_max.py
+++ codes/pred_cal_tmscore_blind_max.py
@@ -29,49 +29,6 @@ class CF_MSA_max():
         print(command)
         os.system(command)
         
-
-
-
-class CF_MSA_var():
-    def __init__(self, pdb1_name, search_dir, output_dir, rseed):
-        #### shallow MSA section
-        #### Global viarlable
-        max_msa = 1
-        ext_msa = 2
-        random_seed = np.array(rseed) ## needed to remove future
-
-        self.pdb1_name = pdb1_name
-
-
-
-        for ran_seed in random_seed:
-            max_msa = 1
-            ext_msa = 2
-
-            TMscores_random = []
-
-            for multi in (1, 2, 2, 2, 2, 2, 2):
-                max_msa = max_msa * multi
-                ext_msa = ext_msa * multi
-
-                #### Colabfold part
-                #command = 'colabfold_batch --model-type alphafold2 --num-seed 5 --max-seq ' + str(max_msa) + ' --max-extra-seq ' + str(ext_msa) + search_dir + output_dir + str(ran_seed) + '_max_' + str(max_msa) + '_ext_' + str(ext_msa)
-                command = 'colabfold_batch --num-seed 5 --max-seq ' + str(max_msa) + ' --max-extra-seq ' + str(ext_msa) + search_dir + output_dir + str(ran_seed) + '_max_' + str(max_msa) + '_ext_' + str(ext_msa)
-                print(command)
-                os.system(command)
-
-
-
-            fin_pred_dir = pdb1_name + '_predicted_models_rand_' + str(ran_seed) + '_max_*'
-            gen_dir =  pdb1_name
-
-            if not os.path.exists(gen_dir):
-                os.makedirs(gen_dir)
-                mv_command = 'mv ' + fin_pred_dir + ' ' + pdb1_name
-                print(mv_command); os.system(mv_command)
-            else:
-                mv_command = 'mv ' + fin_pred_dir + ' ' + pdb1_name
-                print(mv_command); os.system(mv_command)
                 
 
 
@@ -104,11 +61,6 @@ class prediction_all_blind_max():
         print(mv_folder_cmd); os.system(mv_folder_cmd)
 
 
-
-        ###### check out varied-MSA with (msa-max: 1, 2, 4, 8, 16, 32, 64) (msa-extra: 2, 4, 8, 16, 32, 64, 128)
-        #output_dir = ' ' + pdb1_name + '_predicted_models_rand_'
-        #random_seed = random.sample(range(100), 1)
-        #MSA_var = CF_MSA_var(pdb1_name, search_dir, output_dir, random_seed)
 
 
 
